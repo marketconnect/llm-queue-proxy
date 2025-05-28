fcbba33e39c3df283c017b64d5e9299b20a4ce10
marketconnect <ipbogachenko@yandex.ru>
2025-05-28 22:19:39 +0300

    initital

--- app/internal/handlers/proxy.go
+++ app/internal/handlers/proxy.go
@@ -2,15 +2,20 @@ package handlers
 
 import (
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/marketconnect/llm-queue-proxy/app/internal/queue"
 )
 
 func ProxyHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Handling request for: %s", r.URL.String())
+	// ...
+
 	body, _ := io.ReadAll(r.Body)
 	r.Body.Close()
 	r.Body = io.NopCloser(io.NopCloser(nil)) // prevent reuse
 
+	log.Printf("Got response: %s", string(body))
 	queue.Push(queue.ProxyRequest{W: w, R: r, Body: body})
 }
